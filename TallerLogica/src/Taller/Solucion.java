9a3338e8fac42cf0ff4da8900f6dc5856cbb5a11
AlexbyHrdz <alexby11ehn@gmail.com>
2023-08-15 16:57:40 -0500

    TallerLogica
    Avanzado

--- TallerLogica/src/Taller/Solucion.java
+++ TallerLogica/src/Taller/Solucion.java
@@ -10,7 +10,34 @@ package Taller;
  */
 public class Solucion {
     public static void main(String[]args){
-        System.out.println("Hola mundo ");
+        double a= 3+(8*5)-(6/3);
+        System.out.println("Resultado "+a);
+
+        //ejercicio2
+        double b= (2.5*2*3)-(4/2)+8f;
+        System.out.println("resultado 2: " + b);
+        //ejercicio 3
+        
+        double c= 2*(6-2.5)/3;
+        System.out.println("Resultado 3: " + c);
+
+        //ejercicio 4
+         boolean d = 25>18;
+         System.out.println("Resultado 4: "+ d);
+         
+         //ejercicio 5
+         double num1 = 3+(2.5*4)/3 ;
+                 double num2= (4*5.2)-3.8;
+                 boolean res = num1 != num2;
+                 System.out.println("Si los valores son diferentes dice true: " +res);
+                 
+        //ejercicio 6
+        double pri = (8.5/3.2+6.5-2.3*5/6  )  ;
+        double h = 15.8;
+        boolean j = pri >= h;
+        double seg = (21.4<81/4+2);
+                 
+           3 >= 15
     }
     
 }
